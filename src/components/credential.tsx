59fb3b490d644e17766b965f6d24975e7cffbe00
Lennon Araujo <lennon.araujo@corebiz.ag>
2024-04-11 16:56:44 -0300

    animation for credential with Moti and animated

--- src/components/credential.tsx
+++ src/components/credential.tsx
@@ -1,9 +1,10 @@
-import { Image, ImageBackground, Text, TouchableOpacity, View } from "react-native";
+import { Image, ImageBackground, Text, TouchableOpacity, View, useWindowDimensions } from "react-native";
 import { Feather } from "@expo/vector-icons";
 
 import { colors } from "@/styles/colors";
 import { QRCode } from "@/components/qrcode";
 import { BadgeStore } from "@/store/badge-store";
+import { MotiView } from "moti"
 
 type CredentialProps = {
   data: BadgeStore
@@ -12,8 +13,33 @@ type CredentialProps = {
 }
 
 export function Credential({onChangeAvatar, onShowQRCode, data}: CredentialProps) {
+  const { height } = useWindowDimensions()
   return (
-    <View className="w-full self-stretch items-center">
+    <MotiView
+      className="w-full self-stretch items-center"
+      from={{
+        opacity: 0,
+        translateY: -height,
+        rotateZ: "50deg",
+        rotateY: "30deg",
+        rotateX: "30deg",
+       }}
+      animate={{
+        opacity: 1,
+        translateY: 0,
+        rotateZ: "0deg",
+        rotateY: "0deg",
+        rotateX: "0deg"
+      }}
+      transition={{
+        type: "spring",
+        damping: 20,
+        rotateZ: {
+          damping: 15,
+          mass: 3
+        }
+      }}
+    >
       <Image
         source={require('@/assets/ticket/band.png')}
         className="w-24 h-52 z-10"
@@ -72,6 +98,6 @@ export function Credential({onChangeAvatar, onShowQRCode, data}: CredentialProps
           </Text>
         </TouchableOpacity>
       </View>
-    </View>
+    </MotiView>
   )
 }
