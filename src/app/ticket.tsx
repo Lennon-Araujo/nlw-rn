9d9c9675b728543825f00a10bc4ed80a1244c81e
Lennon Araujo <lennon.araujo@corebiz.ag>
2024-04-11 17:07:06 -0300

    animation for doublearrow

--- src/app/ticket.tsx
+++ src/app/ticket.tsx
@@ -3,6 +3,7 @@ import { Alert, Modal, ScrollView, Share, StatusBar, Text, TouchableOpacity, Vie
 import { FontAwesome } from "@expo/vector-icons";
 import * as ImagePicker from 'expo-image-picker';
 import { Redirect } from "expo-router";
+import { MotiView } from 'moti'
 
 import { colors } from "@/styles/colors";
 import { Header } from "@/components/header";
@@ -68,12 +69,26 @@ export default function Ticket() {
           data={badgeStore.data}
         />
 
-        <FontAwesome
-          name="angle-double-down"
-          size={24}
-          color={colors.gray[300]}
-          className="self-center my-6"
-        />
+        <MotiView
+          from={{
+            translateY: 0
+          }}
+          animate={{
+            translateY: 10
+          }}
+          transition={{
+            loop: true,
+            type: "timing",
+            duration: 700
+          }}
+        >
+          <FontAwesome
+            name="angle-double-down"
+            size={24}
+            color={colors.gray[300]}
+            className="self-center my-6"
+          />
+        </MotiView>
 
         <Text className="text-white font-bold text-2xl mt-4">
           Compartilhar credencial
